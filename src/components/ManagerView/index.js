1e69fa56fce8356d2e04c098109cb02fb7413a50
grafijoss <grafijoss@hotmail.com>
2020-10-07 17:48:19 -0500

    prevNext

---
+++ src/components/ManagerView/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const ManagerView = () => {
+  return (
+    <div className="card-container">
+      <h1>Manager View</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla efficitur
+        euismod diam sed rutrum. Curabitur vitae sem vitae lacus ullamcorper
+        sollicitudin.
+      </p>
+    </div>
+  );
+};
