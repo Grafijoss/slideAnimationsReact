78dd259794fd36d152285169fbfd6a15c7f29d44
josemontenegro <josemontenegro@zemoga.com>
2020-10-07 23:27:50 -0500

    steps

--- src/components/ManagerView/index.js
+++ src/components/ManagerView/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {Step1} from './Step1'
+import {Step2} from './Step2'
+import {Step3} from './Step3'
 
 import { WrrpSlider, Slide, WrppButtons } from './style'
 
@@ -8,7 +10,8 @@ export const ManagerView = () => {
   const [stepSlide, setStepSlide] = useState({ type: null, initial: true })
   const [currentSlide, setCurrentSlide] = useState('prev')
   const [finishSlide, setfinishSlide] = useState('enter')
-
+  const [indexStep, setIndexStep] = useState(1)
+  
   useEffect(function () {
     const $step1 = document.getElementById('step1')
     $step1.addEventListener('animationend', (event) => {
@@ -16,6 +19,7 @@ export const ManagerView = () => {
       // setCurrentSlide(nextSlide)
       // setIsAnimation(false)
       if (finishSlide === 'exit') {
+        console.log(currentSlide)
         setfinishSlide('enter')
         if (currentSlide === 'prev') {
           setStepSlide({
@@ -34,17 +38,24 @@ export const ManagerView = () => {
     })
   })
 
-  const moveSlide = (value) => {
+  const moveStep = (value) => {
+    
     if (!isAnimation) {
       setIsAnimation(true)
       setCurrentSlide(value)
       setfinishSlide('exit')
       if (value === 'prev') {
-        setStepSlide({
-          type: 'exitLeft',
-          initial: false,
-        })
+        if (indexStep > 1) {
+          setIndexStep(indexStep-1)
+          setStepSlide({
+            type: 'exitLeft',
+            initial: false,
+          })
+        } else {
+          alert('termino')
+        }
       } else {
+        setIndexStep(indexStep+1)
         setStepSlide({
           type: 'exitRight',
           initial: false,
@@ -57,12 +68,14 @@ export const ManagerView = () => {
     <div className="card-container">
       <h1>Manager View</h1>
       <WrppButtons>
-        <button onClick={() => moveSlide('prev') }>Prev</button>
-        <button onClick={() => moveSlide('next') }>Next</button>
+      <button onClick={() => moveStep('prev') }>Prev</button>
+        {indexStep < 3 && <button onClick={() => moveStep('next') }>Next</button>}
       </WrppButtons>
       <WrrpSlider>
         <Slide id="step1" typeAnimation={stepSlide}>
-          <Step1 />
+          {indexStep === 1 && <Step1/>}
+          {indexStep === 2 && <Step2/>}
+          {indexStep === 3 && <Step3/>}
         </Slide>
       </WrrpSlider>
     </div>
